b6c951663a892c8f8eb8e7bec9cb5f646276a0d8
= <vetle.dyngeland@gmail.com>
2023-06-17 13:51:37 +0200

    remove since it did not work with gpt-3-turbo

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -46,31 +46,4 @@ return {
             vim.cmd [[colorscheme catppuccin-macchiato]]
         end
     },
-    {
-        "svermeulen/text-to-colorscheme.nvim",
-        config = function()
-            local color = require("text-to-colorscheme")
-
-            color.setup({
-                ai = {
-                    openai_api_key = vim.env.OPENAI_API_KEY,
-                    gpt_model = "gpt-3.5-turbo",
-                }
-            })
-
-            local function opts(description)
-                return { noremap = true, silent = true, desc = description }
-            end
-
-            local map = vim.keymap.set
-
-            map("n", "<F8>", "<cmd>T2CShuffleAccents<cr>", opts("T2C: shuffle accents"))
-
-            map("n", "<F9>", "<cmd>T2CAddContrast -0.1<cr>", opts("T2C: decrease contrast"))
-            map("n", "<F10>", "<cmd>T2CAddContrast 0.1<cr>", opts("T2C: increase contrast"))
-
-            map("n", "<F11>", "<cmd>T2CAddSaturation -0.1<cr>", opts("T2C: decrease saturation"))
-            map("n", "<F12>", "<cmd>T2CAddSaturation 0.1<cr>", opts("T2C: increase saturation"))
-        end
-    }
 }
