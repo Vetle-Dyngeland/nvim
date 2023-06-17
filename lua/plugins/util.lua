a1f3392b28fa6c6d72e69b30a2ed16b0146dcb43
= <vetle.dyngeland@gmail.com>
2023-06-17 13:58:37 +0200

    add chat gpt

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -37,5 +37,21 @@ return {
         config = function()
             require("todo-comments").setup()
         end
+    },
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
+        },
+        config = function()
+            local chat = require("chatgpt")
+
+            chat.setup({
+                api_key_cmd = vim.env.OPENAI_API_KEY,
+            })
+        end
     }
 }
