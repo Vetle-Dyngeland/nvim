9161ff0abe6de4cb268d14049484ada0e0ed93ca
= <vetle.dyngeland@gmail.com>
2023-06-28 20:40:16 +0200

    more lazygit

--- lua/plugins/other/lazygit.lua
+++ lua/plugins/other/lazygit.lua
@@ -2,5 +2,9 @@ return {
     "kdheepak/lazygit.nvim",
     config = function()
         require("telescope").load_extension("lazygit")
+
+        local map = vim.keymap.set
+
+        map("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open lazygit" })
     end
 }
